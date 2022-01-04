52aed1328b7b7b8d1210a7aa3a6cace46b597dea
Yosri Hammadi <yosri.hammadi@gmail.com>
2022-01-04 16:46:03 +0100

    create form-input component and custom button for form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App(props) {
   return (
@@ -12,6 +13,7 @@ function App(props) {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/shop" element={<Shop />} />
+        <Route exact path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
