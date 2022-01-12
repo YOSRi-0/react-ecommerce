75e1fcc10c996cdec2d1afd4597dac249cef371d
Yosri Hammadi <yosri.hammadi@gmail.com>
2022-01-12 21:03:57 +0100

    create stripe button, integrate stripe test payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JuESlI01Unj5Zu7eZfIgu2ZTeIryYlht5LHpTPnnbPhJsmREcVDvLL2NpwjXOhUdN0jtawUuwAMVbdKPiQqOTlv00jJyueWO7';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful!');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
