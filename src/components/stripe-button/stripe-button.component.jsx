24423ca0423397601fcfe30f9f2964e505f5f298
Yosri Hammadi <yosri.hammadi@gmail.com>
2022-01-13 16:50:40 +0100

    Revert "update favicon and title, modify price style"
    This reverts commit 8ba31ac12484073a7cdf94cff36a4a464e69955d.

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -14,7 +14,7 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label="Pay Now"
-      name="BEAR Clothing Ltd."
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
