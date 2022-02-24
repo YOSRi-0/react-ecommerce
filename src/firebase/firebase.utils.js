738fceed11b13603baeb25d05aae80a7df972b83
Yosri Hammadi <yosri.hammadi@gmail.com>
2022-02-24 15:37:25 +0100

    recreating current user persistence with saga generic function

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -77,6 +77,15 @@ export const convertCollectionsSnapshopToMap = (collections) => {
 
 firebase.initializeApp(config);
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
